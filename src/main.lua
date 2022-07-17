e9e05d52138c99828b5805edb430088b3aae673f
Beetroot Paul <paul@beetrootpaul.com>
2022-07-18 00:58:53 +0200

    buffer one button press during movement animation

--- src/main.lua
+++ src/main.lua
@@ -62,6 +62,7 @@ function new_player(params)
         }),
         movement1 = nil,
         movement2 = nil,
+        buffered_button = nil,
     }
 
     return player
@@ -79,77 +80,96 @@ function _update60()
     player.walk_animation.advance_1_frame()
 
     if player.movement1 then
+        for button, _ in pairs(u.buttons_to_directions) do
+            if btnp(button) then
+                player.buffered_button = button
+            end
+        end
         if player.movement1.has_finished then
             player.movement1 = nil
         else
             player.movement1.advance_1_frame()
         end
     elseif player.movement2 then
+        for button, _ in pairs(u.buttons_to_directions) do
+            if btnp(button) then
+                player.buffered_button = button
+            end
+        end
         if player.movement2.has_finished then
             player.movement2 = nil
         else
             player.movement2.advance_1_frame()
         end
     else
-        for button, direction in pairs(u.buttons_to_directions) do
-            if btnp(button) then
-                local next_x, next_y = player.x_tile + direction.x, player.y_tile + direction.y
-                local tile = mget(u.levels[level].map_position.x_tile + next_x, u.levels[level].map_position.y_tile + next_y)
-                if tile == u.sprites.level_exit then
-                    if level < #u.levels then
-                        level = level + 1
-                        player = new_player({
-                            x_tile = u.levels[level].player_start.x_tile,
-                            y_tile = u.levels[level].player_start.y_tile,
-                        })
-                    else
-                        extcmd("reset")
-                    end
-                    return
+        local button_to_handle = player.buffered_button
+        if button_to_handle then
+            player.buffered_button = nil
+        else
+            for button, _ in pairs(u.buttons_to_directions) do
+                if btnp(button) then
+                    button_to_handle = button
                 end
-                if fget(tile, u.flags.non_walkable) then
-                    player.movement1 = new_movement({
-                        start_x = player.x_tile * u.tile_edge_length,
-                        start_y = player.y_tile * u.tile_edge_length,
-                        end_x = player.x_tile * u.tile_edge_length + (next_x * u.tile_edge_length - player.x_tile * u.tile_edge_length) / 4,
-                        end_y = player.y_tile * u.tile_edge_length + (next_y * u.tile_edge_length - player.y_tile * u.tile_edge_length) / 4,
+            end
+        end
+        if button_to_handle then
+            local direction = u.buttons_to_directions[button_to_handle]
+            local next_x, next_y = player.x_tile + direction.x, player.y_tile + direction.y
+            local tile = mget(u.levels[level].map_position.x_tile + next_x, u.levels[level].map_position.y_tile + next_y)
+            if tile == u.sprites.level_exit then
+                if level < #u.levels then
+                    level = level + 1
+                    player = new_player({
+                        x_tile = u.levels[level].player_start.x_tile,
+                        y_tile = u.levels[level].player_start.y_tile,
                     })
-                    player.movement2 = new_movement({
-                        start_x = player.x_tile * u.tile_edge_length + (next_x * u.tile_edge_length - player.x_tile * u.tile_edge_length) / 4,
-                        start_y = player.y_tile * u.tile_edge_length + (next_y * u.tile_edge_length - player.y_tile * u.tile_edge_length) / 4,
-                        end_x = player.x_tile * u.tile_edge_length,
-                        end_y = player.y_tile * u.tile_edge_length,
-                    })
-                    if direction.x > 0 then
-                        player.facing_right = true
-                    elseif direction.x < 0 then
-                        player.facing_right = false
-                    end
-                    if tile == u.sprites.door then
-                        sfx(u.sounds.door_open_sfx)
-                        mset(u.levels[level].map_position.x_tile + next_x, u.levels[level].map_position.y_tile + next_y, u.sprites.floor)
-                    elseif tile == u.sprites.chest.closed then
-                        sfx(u.sounds.chest_open_sfx)
-                        mset(u.levels[level].map_position.x_tile + next_x, u.levels[level].map_position.y_tile + next_y, u.sprites.chest.open)
-                    else
-                        sfx(u.sounds.wall_bump_sfx)
-                    end
                 else
-                    player.movement1 = new_movement({
-                        start_x = player.x_tile * u.tile_edge_length,
-                        start_y = player.y_tile * u.tile_edge_length,
-                        end_x = next_x * u.tile_edge_length,
-                        end_y = next_y * u.tile_edge_length,
-                    })
-                    player.x_tile = next_x
-                    player.y_tile = next_y
-                    if direction.x > 0 then
-                        player.facing_right = true
-                    elseif direction.x < 0 then
-                        player.facing_right = false
-                    end
-                    sfx(u.sounds.walk_step_sfx)
+                    extcmd("reset")
                 end
+                return
+            end
+            if fget(tile, u.flags.non_walkable) then
+                player.movement1 = new_movement({
+                    start_x = player.x_tile * u.tile_edge_length,
+                    start_y = player.y_tile * u.tile_edge_length,
+                    end_x = player.x_tile * u.tile_edge_length + (next_x * u.tile_edge_length - player.x_tile * u.tile_edge_length) / 4,
+                    end_y = player.y_tile * u.tile_edge_length + (next_y * u.tile_edge_length - player.y_tile * u.tile_edge_length) / 4,
+                })
+                player.movement2 = new_movement({
+                    start_x = player.x_tile * u.tile_edge_length + (next_x * u.tile_edge_length - player.x_tile * u.tile_edge_length) / 4,
+                    start_y = player.y_tile * u.tile_edge_length + (next_y * u.tile_edge_length - player.y_tile * u.tile_edge_length) / 4,
+                    end_x = player.x_tile * u.tile_edge_length,
+                    end_y = player.y_tile * u.tile_edge_length,
+                })
+                if direction.x > 0 then
+                    player.facing_right = true
+                elseif direction.x < 0 then
+                    player.facing_right = false
+                end
+                if tile == u.sprites.door then
+                    sfx(u.sounds.door_open_sfx)
+                    mset(u.levels[level].map_position.x_tile + next_x, u.levels[level].map_position.y_tile + next_y, u.sprites.floor)
+                elseif tile == u.sprites.chest.closed then
+                    sfx(u.sounds.chest_open_sfx)
+                    mset(u.levels[level].map_position.x_tile + next_x, u.levels[level].map_position.y_tile + next_y, u.sprites.chest.open)
+                else
+                    sfx(u.sounds.wall_bump_sfx)
+                end
+            else
+                player.movement1 = new_movement({
+                    start_x = player.x_tile * u.tile_edge_length,
+                    start_y = player.y_tile * u.tile_edge_length,
+                    end_x = next_x * u.tile_edge_length,
+                    end_y = next_y * u.tile_edge_length,
+                })
+                player.x_tile = next_x
+                player.y_tile = next_y
+                if direction.x > 0 then
+                    player.facing_right = true
+                elseif direction.x < 0 then
+                    player.facing_right = false
+                end
+                sfx(u.sounds.walk_step_sfx)
             end
         end
     end
@@ -193,8 +213,6 @@ end
 -- TODO: interaction: vase -> no vase
 -- TODO: interaction: stone tablet -> (nothing yet)
 
--- TODO: next button 1-slot buffer to be able to move again during movement animation
-
 -- TODO: SFX for chest open, but no space in inventory
 -- TODO: SFX for vase break
 
@@ -205,6 +223,3 @@ end
 -- TODO: window disappear on X press
 -- TODO: X animated (`SIN(TIME())` might be helpful)
 -- TODO: multiple stone tablets with their texts (maybe use last sprite flags as binary number of text?)
-
--- TODO: level end on step on end stairs
-
